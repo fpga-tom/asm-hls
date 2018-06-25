16e2267266bc3ae9fa29c48c6887c5c754331a1e
tomas <tomaskrajco@gmail.com>
2018-06-25 21:58:46 +0200

    fir

--- b.asm
+++ b.asm
@@ -1,4 +1,7 @@
 l0:
+    add r11, r10, r8
+    add r10, r9, r7
+    add r9, r5, r6
     mul r8, r4, 4
     mul r7, r3, 3
     mul r6, r2, 2
@@ -7,7 +10,4 @@ l0:
     mov r3, r2
     mov r2, r1
     mov r1, r0
-    add r9, r8, r7
-    add r10, r6, r5
-    add r11, r10, r9
     jmp l0
