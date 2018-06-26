3aa3a94417221aa4511910033394b9a5280c4dd3
tomas <tomaskrajco@gmail.com>
2018-06-26 21:36:37 +0200

    assembly

--- a.asm
+++ a.asm
@@ -1,56 +1,38 @@
-alias mar = r0[15:0]
-alias pc = r1[15:0]
-alias rd = r2[0:0]
-alias dr = r3[0:0]
-alias ir = r4[8:0]
-alias opd = r12[15:0]
-alias acc = r5[8:0]
-alias carry = r6[0:0]
-alias x = r7[7:0]
-alias y = r8[7:0]
-alias sr = r9[7:0]
-alias sp = r10[7:0]
-alias mdr = r11[7:0]
-alias ll = r100[7:0]
-alias hh = r101[7:0]
+reg carry
+reg rd
+reg dr
+reg [7:0] ir
+reg [7:0] acc
+reg [7:0] x
+reg [7:0] y
+reg [7:0] sr
+reg [7:0] sp
+reg [15:0] pc
+reg [15:0] opd
+reg [15:0] mdr
+reg [15:0] mar
+reg [7:0] mem0  [8191:0]
 ###################################
-macro d_read
-    mov rd, 1
-l0:
-    jne dr, 1, l0
-    mov rd, 0
-
-macro pc_read
-    mov mar, pc
-    d_read
-
-macro pc_read_inc
-    add pc, pc, 1
-    pc_read
-
 macro imm_read
-    pc_read_inc
+    add pc, pc, 1
+    ld mdr, mem0, pc
 
 macro abs_read
-    pc_read_inc
-    mov ll, mdr
-    pc_read_inc
-    mov hh, mdr
+    add pc, pc, 1
+    ld mdr[7:0], mem0, pc
+    add pc, pc, 1
+    ld mdr[15:8], mem0, pc
 
 macro ind_read
-    abs_read
-    mov mar[0:7], ll
-    mov mar[15:0], hh
-    d_read
+    add pc, pc, 1
+    ld mdr[7:0], mem0, pc
+    add pc, pc, 1
+    ld mdr[15:0], mem0, pc
 
 ###################################
 i_fetch:
-    pc_read
-    mov ir, mdr
-    jmp i_decode
-inc_pc:
+    ld ir, mem0, pc
     add pc, pc, 1
-    jmp i_fetch
 i_decode:
     je ir, 69, op_adc_69_imm
     je ir, 29, op_and_29_imm
@@ -75,12 +57,14 @@ op_asl_0A_acc:
     mov acc[0], 0
     jmp inc_pc
 op_jmp_4C_abs:
-    abs_read
-    mov pc[7:0], ll
-    mov pc[15:8], hh
+    add hi, pc, 1
+    ld pc[7:0], mem0, lo
+    ld pc[15:8], mem0, hi
     jmp i_fetch
 op_jmp_6C_ind:
-    ind_read
-    mov pc[7:0], ll
-    mov pc[15:8], hh
+    ld lo, mem0, pc
+    add mar, pc, 1
+    ld hi, mem0, mar
+    ld pc[15:8], mem0, lo
+    ld pc[7:0], mem0, hi
     jmp i_fetch
