cec7a445bb4c8ad886f83aab0e36f8ada6afaa6a
tomas <tomaskrajco@gmail.com>
2018-06-30 19:31:08 +0200

    cleanup

--- b.asm
+++ b.asm
@@ -1,13 +1,11 @@
-l0:
     add r11, r10, r8
     add r10, r9, r7
     add r9, r5, r6
-    mul r8, r4, 4
-    mul r7, r3, 3
-    mul r6, r2, 2
-    mul r5, r1, 1
+    mul r8, r4, 1
+    mul r7, r3, 2
+    mul r6, r2, 3
+    mul r5, r1, 4
     mov r4, r3
     mov r3, r2
     mov r2, r1
     mov r1, r0
-    jmp l0
